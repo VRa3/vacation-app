57b01a238b82dd2c6dc0c3d94deb5a05ba4534dd
Rafał Janus <rafal.janus.94@gmail.com>
2019-07-08 11:46:38 +0200

    Updates

---
+++ precache-manifest.59e8b1a74b58b74485b8ad715228d828.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "426a161bb7c3b64e3adeef6a9d7e5af0",
+    "url": "/VRa3/vacation-app/index.html"
+  },
+  {
+    "revision": "e7c1f6ed2c6b59223fb3",
+    "url": "/VRa3/vacation-app/static/css/main.6b978d23.chunk.css"
+  },
+  {
+    "revision": "34a209f595754ad3a331",
+    "url": "/VRa3/vacation-app/static/js/2.d812f8a7.chunk.js"
+  },
+  {
+    "revision": "e7c1f6ed2c6b59223fb3",
+    "url": "/VRa3/vacation-app/static/js/main.6c46a87d.chunk.js"
+  },
+  {
+    "revision": "832f3adff0e2c6b6b7b7",
+    "url": "/VRa3/vacation-app/static/js/runtime~main.d8aeb974.js"
+  },
+  {
+    "revision": "8bfd79fb6c0e06bde9db9aba63558e8f",
+    "url": "/VRa3/vacation-app/static/media/man.8bfd79fb.svg"
+  },
+  {
+    "revision": "9e32ff178bd6774946de63049dfdba9a",
+    "url": "/VRa3/vacation-app/static/media/woman.9e32ff17.svg"
+  }
+]);
